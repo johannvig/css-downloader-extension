9f25bc6a3a6a612844004ffcd5ae394b161a4b42
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2024-07-03 15:31:45 +0200

    Create background.js

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ['content.js']
+    });
+  }
+});
